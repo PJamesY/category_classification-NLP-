35ac0fe4cfee78f856bad481baac95be4da56166
PJamesY <junyoungcat@gmail.com>
2019-05-15 12:51:32 +0900

    add MakeCnnDataset class

--- make_cnn_dataset.py
+++ make_cnn_dataset.py
@@ -3,91 +3,109 @@ import pandas as pd
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
+from preprocessing import ArticlePreProcessing
 
 
 # dataframe 가져오기(load)
-df = pd.read_pickle('data/okt_cnn_dataset.pkl')
-
-# array 형태로 string sentence - > ['sentence1','sentence2','...']
-docs = np.array(df['joined_sentence'])
-
-# Constant
-# 한 문장에 사용할 단어 갯수
-def sentence_length(df):
-    max_length = 0
-
-    for word_tag_ls in df['word_tagging']:
-        if len(word_tag_ls) >= max_length:
-            max_length = len(word_tag_ls)
-
-    return max_length
+# preprocessing.py에서 전처리하고 저장한 데이터를 make_cnn_dataset.py로 불러온다
+df = pd.read_csv('data/article.csv')
 
 
-MAX_SEQ_LENGTH = sentence_length(df) # 최대 문장 길이
-MAX_NUM_WORDS = 15000
+class MakeCnnDataset:
 
-def make_data():
-    # tokenizer
-    tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
-    tokenizer.fit_on_texts(docs)
-    sequences = tokenizer.texts_to_sequences(docs)
+    def __init__(self, konlpy="Okt", max_num_words=15000):
+        """
+        :param konlpy: 형태소 분석기 선택 ['Okt', 'Komoran']
+        :param max_num_words: 최대 사용할 단어의 갯
+        """
+        self.konlpy = konlpy
+        self.max_num_words = max_num_words
 
-    word_to_index = tokenizer.word_index
-    vocabulary = word_to_index.keys()
+        # self.docs : string sentence - > ['sentence1','sentence2','...']
+        if self.konlpy == "Okt":
+            preprocessing = ArticlePreProcessing(df, remove=False)
+            self.df = preprocessing.doc2sentence()
+            self.docs = np.array(self.df['joined_sentence'])
+        else:
+            preprocessing = ArticlePreProcessing(df, konlpy='Komoran', remove=False)
+            self.df = preprocessing.doc2sentence()
+            self.docs = np.array(self.df['joined_sentence'])
 
-    sentence = [len(sentence.split()) for sentence in docs]
+    def sentence_length(self):
+        """
+        :return: max length of sentence
+        """
+        max_length = 0
 
-    print('Text informations:')
-    print('sentence max length: %i / min length: %i / mean length: %i / limit length: %i' % (np.max(sentence),
-                                                                                    np.min(sentence),
-                                                                                    np.mean(sentence),
-                                                                                    MAX_SEQ_LENGTH))
-    print('총 단어 갯수: %i / 한 문장 속에 제한: %i' % (len(word_to_index), MAX_NUM_WORDS))
+        for word_tag_ls in self.df['word_tagging']:
+            if len(word_tag_ls) >= max_length:
+                max_length = len(word_tag_ls)
+
+        return max_length
+
+    def make_data(self):
+        # tokenizer
+        tokenizer = Tokenizer(num_words=self.max_num_words)
+        tokenizer.fit_on_texts(self.docs)
+        sequences = tokenizer.texts_to_sequences(self.docs)
+
+        word_to_index = tokenizer.word_index
+        vocabulary = word_to_index.keys()
+
+        sentence = [len(sentence.split()) for sentence in self.docs]
+        max_length = self.sentence_length()
+
+        print('Text informations:')
+        print('sentence max length: %i / min length: %i / mean length: %i / limit length: %i' % (np.max(sentence),
+                                                                                                 np.min(sentence),
+                                                                                                 np.mean(sentence),
+                                                                                                 max_length))
+        print('총 단어 갯수: %i / 한 문장 속에 제한: %i' % (len(word_to_index), self.max_num_words))
+
+        # make X data set
+        # Padding all sequences to same length of `MAX_SEQ_LENGTH`, 최대 문장 길이보다 작은 문장은 나머지 단어들은 padding 0으로 처리 된다
+        X = pad_sequences(sequences, maxlen=max_length, padding='post')
+
+        # X
+        # array([[ 2587,    36,   969, ...,     0,     0,     0],
+        #       [ 1575,     4,   121, ...,     0,     0,     0],
+        #       [    5,     2,  2639, ...,     0,     0,     0],
+        #       ...,
+        #       [  742,  4671, 14415, ...,     0,     0,     0],
+        #       [11686,    61, 14415, ...,     0,     0,     0],
+        #       [14359,   741,     1, ...,     0,     0,     0]], dtype=int32)
+
+        # make y data set
+        cateory_to_label = {'education': 0,
+                            'health': 1,
+                            'politics': 2,
+                            'technology': 3,
+                            'it': 3,
+                            'sports': 4,
+                            'entertainment': 5,
+                            'travel': 6,
+                            }
+
+        # dataframe에 카테고리들을 int label 컬럼을 추가시킨다
+        self.df['category2label'] = self.df['category'].apply(lambda category: cateory_to_label[category])
+
+        y = to_categorical(list(self.df['category2label']))
+
+        # y
+        # array([[0., 0., 0., ..., 0., 1., 0.],
+        #        [1., 0., 0., ..., 0., 0., 0.],
+        #        [0., 1., 0., ..., 0., 0., 0.],
+        #        ...,
+        #        [0., 0., 1., ..., 0., 0., 0.],
+        #        [1., 0., 0., ..., 0., 0., 0.],
+        #        [0., 0., 1., ..., 0., 0., 0.]], dtype=float32)
+        return X, y, word_to_index, vocabulary
 
 
-    # make X data set
-    # Padding all sequences to same length of `MAX_SEQ_LENGTH`, 최대 문장 길이보다 작은 문장은 나머지 단어들은 padding 0으로 처리 된다
-    X = pad_sequences(sequences, maxlen=MAX_SEQ_LENGTH, padding='post')
-
-    # X
-    # array([[ 2587,    36,   969, ...,     0,     0,     0],
-    #       [ 1575,     4,   121, ...,     0,     0,     0],
-    #       [    5,     2,  2639, ...,     0,     0,     0],
-    #       ...,
-    #       [  742,  4671, 14415, ...,     0,     0,     0],
-    #       [11686,    61, 14415, ...,     0,     0,     0],
-    #       [14359,   741,     1, ...,     0,     0,     0]], dtype=int32)
-
-
-    # make y data set
-    cateory_to_label = {'education': 0,
-                        'health': 1,
-                        'politics': 2,
-                        'technology': 3,
-                        'it': 4,
-                        'sports': 5,
-                        'entertainment': 6,
-                        'travel': 7
-                        }
-
-    # dataframe에 카테고리들을 int label 컬럼을 추가시킨다
-    df['category2label'] = df['category'].apply(lambda category: cateory_to_label[category])
-
-    y = to_categorical(list(df['category2label']))
-
-    # y
-    # array([[0., 0., 0., ..., 0., 1., 0.],
-    #        [1., 0., 0., ..., 0., 0., 0.],
-    #        [0., 1., 0., ..., 0., 0., 0.],
-    #        ...,
-    #        [0., 0., 1., ..., 0., 0., 0.],
-    #        [1., 0., 0., ..., 0., 0., 0.],
-    #        [0., 0., 1., ..., 0., 0., 0.]], dtype=float32)
-    return X, y, word_to_index, vocabulary
-
-# if __name__ == "__main__":
-#     X,y,word_to_index, voca = make_data()
-#     print(X.shape)
+if __name__ == "__main__":
+    make_cnn_dataset = MakeCnnDataset("Komoran")
+    X, y, word2index, voca = make_cnn_dataset.make_data()
+    print(X.shape)
 
 
 
