5c2d8ddd03b07d8c7ff683d04ce9b03524679534
PJamesY <junyoungcat@gmail.com>
2019-05-15 18:23:42 +0900

    add CNN class

--- model_cnn.py
+++ model_cnn.py
@@ -3,72 +3,110 @@ from keras.layers import Reshape, Flatten, Dropout, Concatenate
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
-
 from sklearn.model_selection import train_test_split
-import make_cnn_dataset
-
-print("Loading Data ...")
-X, y, word_to_index, vocabulary = make_cnn_dataset.make_data()
-
-# X.shape -> (159119, 252)
-# y.shape -> (159119, 8)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-print('Setting parameter ...')
-# model parameter
-SEQUENCE_LENGTH = X.shape[1] # 252(문장 길이)
-VOCABULARY_SIZE = len(vocabulary) # 50972(단어 갯수)
-EMBEDDING_DIM = 256
-FILTER_SIZES = [3,4,5]
-NUM_FILTERS = 512
-DROP_RATE = 0.5
-CHANNEL = 1 # 채널 갯수 : embding하는 갯수(Word2vec, glovec)
-CLASSES = 8
-
-# Learning parameter
-EPOCHS = 100
-BATCH_SIZE = 30
-PRE_TRAINED_EMBEDDING_LAYER = False
+from make_cnn_dataset import MakeCnnDataset
 
 
-print('creating Model  ...')
-# input layer
-inputs = Input(shape=(SEQUENCE_LENGTH,), dtype='int32')
-# embedding layer
-if PRE_TRAINED_EMBEDDING_LAYER:
-    pass
-else:
-    embedding = Embedding(input_dim=VOCABULARY_SIZE, output_dim=EMBEDDING_DIM, input_length=SEQUENCE_LENGTH)(inputs)
-reshape = Reshape((SEQUENCE_LENGTH, EMBEDDING_DIM, CHANNEL))(embedding)
+class CNN:
 
-# Convolution layer
-conv_0 = Conv2D(NUM_FILTERS, kernel_size=(FILTER_SIZES[0], EMBEDDING_DIM), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-conv_1 = Conv2D(NUM_FILTERS, kernel_size=(FILTER_SIZES[1], EMBEDDING_DIM), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-conv_2 = Conv2D(NUM_FILTERS, kernel_size=(FILTER_SIZES[2], EMBEDDING_DIM), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+    def __init__(self, embedding_dim=256, filter_size=[3, 4, 5], num_filters=512,
+                 drop_rate=0.5, epochs=100, batch_size=30, pre_trained_embedding_layer=False):
+        """
 
-# MAX Pooling
-maxpool_0 = MaxPool2D(pool_size=(SEQUENCE_LENGTH - FILTER_SIZES[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
-maxpool_1 = MaxPool2D(pool_size=(SEQUENCE_LENGTH - FILTER_SIZES[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
-maxpool_2 = MaxPool2D(pool_size=(SEQUENCE_LENGTH - FILTER_SIZES[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+        :param embedding_dim: embedding dimension
+        :param filter_size: filter size list
+        :param num_filters: number of filters
+        :param drop_rate: drop rate
+        :param pre_trained_embedding_layer: whether use embedding_layer or not (boolean)
+        """
+        self.embedding_dim = embedding_dim
+        self.filter_size = filter_size
+        self.num_filters = num_filters
+        self.drop_rate = drop_rate
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.pre_trained_embedding_layer = pre_trained_embedding_layer
 
-# Concatenate, Flatten, Dense
-concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
-flatten = Flatten()(concatenated_tensor)
-dropout = Dropout(DROP_RATE)(flatten)
-output = Dense(units=CLASSES, activation='softmax')(dropout)
+        print("Loading Data ...")
+        cnn_dataset = MakeCnnDataset("Komoran")
+        self.X, self.y, self.word_to_index, self.vocabulary = cnn_dataset.make_data()
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y,
+                                                                                test_size=0.2, random_state=42)
+        # self.X.shape -> (number of sentences, length of sentence)
+        # self.y.shape -> (number of sentences, number of category)
 
-# Create model
-model = Model(inputs=inputs, outputs=output)
+        print("Setting parameter...")
+        self.sentence_length = self.X.shape[1]   # length of sentence
+        self.voca_size = len(self.vocabulary)  # number of words
+        self.channel = 1  # number of channels : embding하는 갯수(Word2vec, glovec)
+        self.classes = 7  # number of class
 
-checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
-adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    def model_construction(self):
+        print('creating model..')
 
-model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
-print('Training_model ....')
-model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=1, callbacks=[checkpoint], validation_data=(X_test, y_test))
+        # input layer
+        inputs = Input(shape=(self.sentence_length,), dtype='int32')
+
+        if self.pre_trained_embedding_layer:
+            pass
+            # embedding = Embedding(len(word_index) + 1,
+            #                             EMBEDDING_DIM,
+            #                             weights=[embedding_matrix],
+            #                             input_length=MAX_SEQUENCE_LENGTH,
+            #                             trainable=False)
+        else:
+            embedding = Embedding(input_dim=self.voca_size,
+                                  output_dim=self.embedding_dim,
+                                  input_length=self.sentence_length)(inputs)
+
+        reshape = Reshape((self.sentence_length, self.embedding_dim, self.channel))(embedding)
+
+        # Convolution layer
+        conv_0 = Conv2D(self.num_filters, kernel_size=(self.filter_size[0], self.embedding_dim), padding='valid',
+                        kernel_initializer='normal', activation='relu')(reshape)
+        conv_1 = Conv2D(self.num_filters, kernel_size=(self.filter_size[1], self.embedding_dim), padding='valid',
+                        kernel_initializer='normal', activation='relu')(reshape)
+        conv_2 = Conv2D(self.num_filters, kernel_size=(self.filter_size[2], self.embedding_dim), padding='valid',
+                        kernel_initializer='normal', activation='relu')(reshape)
+
+        # MAX Pooling
+        maxpool_0 = MaxPool2D(pool_size=(self.sentence_length - self.filter_size[0] + 1, 1),
+                              strides=(1, 1), padding='valid')(conv_0)
+        maxpool_1 = MaxPool2D(pool_size=(self.sentence_length - self.filter_size[1] + 1, 1),
+                              strides=(1, 1), padding='valid')(conv_1)
+        maxpool_2 = MaxPool2D(pool_size=(self.sentence_length - self.filter_size[2] + 1, 1),
+                              strides=(1, 1), padding='valid')(conv_2)
+
+        # Concatenate, Flatten, Dense
+        concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+        flatten = Flatten()(concatenated_tensor)
+        dropout = Dropout(self.drop_rate)(flatten)
+        output = Dense(units=self.classes, activation='softmax')(dropout)
+
+        # Create model
+        model = Model(inputs=inputs, outputs=output)
+
+        checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1,
+                                     save_best_only=True, mode='auto')
+        adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+        model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+        print('Training_model ....')
+        model.fit(self.X_train, self.y_train, batch_size=self.batch_size, epochs=self.epochs,
+                  verbose=1, callbacks=[checkpoint], validation_data=(self.X_test, self.y_test))
+
+        return model
+
+    def save_model(self):
+        model = self.model_construction()
+
+        model_json = model.to_json()
+
+        with open("model.json", "w") as json_file:
+            json_file.write(model_json)
 
 
-model_json = model.to_json()
-with open("model.json", "w") as json_file :
-    json_file.write(model_json)
+if __name__ == "__main__":
+    cnn_model = CNN()
+    cnn_model.save_model()
